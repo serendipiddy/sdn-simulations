6b3d0dfcd86b0a682257939c9bf1722aee35049d
iddy <lemon.and.lime@hotmail.com>
2018-04-27 21:07:12 +1200

    added NEDs, class and ini for jarschel's simple queueing model

---
+++ gotos_model/src/JarschelSwitchQueue.cc
@@ -0,0 +1,68 @@
+/*
+ * JarscheSwitch.cc
+ *
+ *  Created on: 27/04/2018
+ *      Author: iddy
+ */
+
+#include "JarschelSwitchQueue.h"
+#include "Job.h"
+
+Define_Module(JarschelSwitchQueue);
+// Register_Class(JarschelSwitchQueue);
+
+void JarschelSwitchQueue::initialize()
+{
+    Queue::initialize();
+    std::cout << "HELLO" << endl;
+    probability_visit_controller = registerSignal("prob_ctrl");
+}
+
+/*
+ * Returns a uniform random number between min and max
+ * http://stackoverflow.com/a/2704552
+ */
+double JarschelSwitchQueue::uniformRand(double min, double max)
+{
+    double f = (double) rand() / RAND_MAX;
+    return min + f*(max - min);
+}
+
+/*
+ * Returns the decision variable for this job visiting the controller.
+ * True means this job will visit the controller when it exits the queue
+ */
+bool JarschelSwitchQueue::visitController(Job *job)
+{
+    double rv = par("probVisitController").doubleValue();
+    bool visited = rv > uniformRand(0,1);
+    if (visited)
+    {
+        EV << "Packet will visit controller " << rv << endl;
+    }
+    else
+    {
+        EV << "Packet won't visit controller " << rv << endl;
+    }
+
+    return visited;
+}
+
+void JarschelSwitchQueue::endService(Job *job)
+{
+    // Queue::endService(job);
+    EV << "Finishing service of " << job->getName() << endl;
+    simtime_t d = simTime() - job->getTimestamp();
+    job->setTotalServiceTime(job->getTotalServiceTime() + d);
+
+    bool visitedController = visitController(job);
+    if (visitedController)
+    {
+        send(job, "out_controller");
+    }
+    else
+    {
+        send(job, "out");
+    }
+}
+
---
+++ gotos_model/src/JarschelSwitchQueue.h
@@ -0,0 +1,36 @@
+/*
+ * JarschelSwitchQueue.h
+ *
+ *  Created on: 27/04/2018
+ *      Author: iddy
+ */
+
+#ifndef JARSCHELSWITCHQUEUE_H_
+#define JARSCHELSWITCHQUEUE_H_
+
+#include <omnetpp.h>
+#include "Queue.h"
+#include "QueueingDefs.h"
+
+using namespace queueing;
+// using namespace omnetpp;
+
+/**
+ * JarschelSwitchQueue module that splits outgoing traffic between two output ports, given a random decision variable.
+ */
+class JarschelSwitchQueue : public queueing::Queue
+{
+  private:
+    simsignal_t toControllerSignal;
+    simsignal_t toDataplaneSignal;
+    double probability_visit_controller;
+  protected:
+    virtual void initialize() override;
+    // virtual void handleMessage(omnetpp::cMessage *msg) override;
+    virtual void endService(Job *job) override;
+
+    virtual bool visitController(Job *job);
+    virtual double uniformRand(double min, double max);
+};
+
+#endif /* JARSCHELSWITCHQUEUE_H_ */
