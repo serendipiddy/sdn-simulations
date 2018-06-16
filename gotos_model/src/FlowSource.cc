80d3e4bc7199a8535565defd1e8c3bae145a7e6a
iddy <lemon.and.lime@hotmail.com>
2018-06-17 02:42:34 +1200

    added flow source object -- not yet a NED though

---
+++ gotos_model/src/FlowSource.cc
@@ -0,0 +1,64 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "FlowSource.h"
+
+namespace queueing {
+
+void FlowSource::initialize()
+{
+    SourceBase::initialize();
+    startTime = par("startTime");
+    stopTime = par("stopTime");
+    numJobs = par("numJobs");
+
+    // schedule the first message timer for start time
+    scheduleAt(startTime, new cMessage("newJobTimer"));
+
+    flowCounter = 0;
+}
+
+void FlowSource::handleMessage(cMessage *msg)
+{
+    ASSERT(msg->isSelfMessage());
+
+    if ((numJobs < 0 || numJobs > jobCounter) && (stopTime < 0 || stopTime > simTime())) {
+        // reschedule the timer for the next message
+        scheduleAt(simTime() + par("interArrivalTime").doubleValue(), msg);
+
+        queueing::Job *job = createJob();
+        send(job, "out");
+    }
+    else {
+        // finished
+        delete msg;
+    }
+}
+
+long FlowSource::getNextFlowID() {
+    return flowCounter++;
+}
+
+queueing::Job *FlowSource::createJob() {
+    char buf[80];
+    sprintf(buf, "%.60s-%d", jobName.c_str(), ++jobCounter);
+    FlowJob *job = new FlowJob(buf);
+    job->setKind(par("jobType"));
+    job->setPriority(par("jobPriority"));
+    job->setFlowID(getNextFlowID());
+    return job;
+}
+
+}; //namespace
